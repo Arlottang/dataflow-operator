b42a2e8bc752ed973db592647d5d730ebf297b39
tangdahe <dahe.tang@pingcap.com>
2022-05-31 17:41:17 +0800

    test for dataflow engine 1m3e demo, test success

--- README.md
+++ README.md
@@ -162,6 +162,27 @@ kubectl run mysql-client -n dev --image=mysql:5.7 -i -t --rm --restart=Never --\
     kubectl delete -f config/samples/dataflow_v1_1m1e_demo.yaml
 ```
 
+- Test for 1m3e Demo
+
+```shell
+    kubectl apply -f config/samples/dataflow_v1_1m3e_demo.yaml
+```
+
+```shell
+    watch kubectl get all -n dev
+```
+
+![](./photo/1m3e_demo.jpeg)
+
+```shell
+    kubectl describe pod <pod_name> -n dev
+    kubectl logs <pod_name> -n dev
+```
+
+```shell
+    kubectl delete -f config/samples/dataflow_v1_1m1e_demo.yaml
+```
+
 6. Verify
 
 ```shell
--- controllers/executor_cluster.go
+++ controllers/executor_cluster.go
@@ -5,6 +5,7 @@ import (
 	dataflowv1 "github.com/StepOnce7/dataflow-operator/api/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -15,10 +16,25 @@ func (r *DataflowEngineReconciler) ReconcileExecutorCluster(ctx context.Context,
 
 	logg := log.FromContext(ctx)
 
+	var cfm corev1.ConfigMap
+	cfm.Name = "executorcfm"
+	cfm.Namespace = instance.Namespace
+
+	or, err := ctrl.CreateOrUpdate(ctx, r.Client, &cfm, func() error {
+		createExecutorConfigMap(&cfm)
+		return controllerutil.SetControllerReference(instance, &cfm, r.Scheme)
+	})
+
+	if err != nil {
+		logg.Error(err, "create executor configMap error")
+		return ctrl.Result{}, err
+	}
+	logg.Info("CreateOrUpdate", "Executor ConfigMap", or)
+
 	var svc corev1.Service
 	svc.Name = instance.Spec.Executor.Name
 	svc.Namespace = instance.Namespace
-	or, err := ctrl.CreateOrUpdate(ctx, r.Client, &svc, func() error {
+	or, err = ctrl.CreateOrUpdate(ctx, r.Client, &svc, func() error {
 		ExecutorHeadlessService(instance, &svc)
 		return controllerutil.SetControllerReference(instance, &svc, r.Scheme)
 	})
@@ -43,6 +59,8 @@ func (r *DataflowEngineReconciler) ReconcileExecutorCluster(ctx context.Context,
 		return ctrl.Result{}, err
 	}
 
+	logg.Info("CreateOrUpdate", "Executor StatefulSet", or)
+
 	logg.Info("dataflow engine executor reconcile end", "reconcile", "success")
 
 	return ctrl.Result{}, nil
@@ -54,15 +72,15 @@ func ExecutorHeadlessService(de *dataflowv1.DataflowEngine, svc *corev1.Service)
 		DataflowEngineCommonLabelKey: "engine",
 	}
 
-	// todo: need more info for ports
 	svc.Spec = corev1.ServiceSpec{
+		ClusterIP: corev1.ClusterIPNone,
 		Selector: map[string]string{
-			ExecutorClusterCommonLabelKey: "engine-executor",
+			ExecutorClusterCommonLabelKey: "executor",
 		},
 		Ports: []corev1.ServicePort{
 			{
-				Name:     "worker-addr",
-				Port:     10241,
+				Name:     "worker",
+				Port:     de.Spec.Executor.Ports,
 				Protocol: corev1.ProtocolTCP,
 			},
 		},
@@ -78,7 +96,7 @@ func ExecutorStatefulSet(de *dataflowv1.DataflowEngine, sts *appsv1.StatefulSet)
 		Replicas: de.Spec.Executor.Size,
 		Selector: &metav1.LabelSelector{
 			MatchLabels: map[string]string{
-				ExecutorClusterCommonLabelKey: "engine-executor",
+				ExecutorClusterCommonLabelKey: "executor",
 			},
 		},
 		ServiceName: de.Spec.Executor.Name,
@@ -86,11 +104,54 @@ func ExecutorStatefulSet(de *dataflowv1.DataflowEngine, sts *appsv1.StatefulSet)
 			ObjectMeta: metav1.ObjectMeta{
 				Labels: map[string]string{
 					DataflowEngineCommonLabelKey:  "engine",
-					ExecutorClusterCommonLabelKey: "engine-executor",
+					ExecutorClusterCommonLabelKey: "executor",
 				},
 			},
 			Spec: corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{
+						Name:  "init-master",
+						Image: "busybox",
+						Command: []string{
+							"sh", "-c",
+							`until nslookup server-master; 
+do 
+	echo waiting for server-master; 
+	sleep 2; 
+done;`,
+						},
+					},
+				},
 				Containers: newExecutorContainer(de),
+				Volumes: []corev1.Volume{
+					{
+						Name: "config-map",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: "executorcfm",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "dataflow",
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("1G"),
+						},
+					},
+				},
 			},
 		},
 	}
@@ -99,16 +160,49 @@ func ExecutorStatefulSet(de *dataflowv1.DataflowEngine, sts *appsv1.StatefulSet)
 
 func newExecutorContainer(de *dataflowv1.DataflowEngine) []corev1.Container {
 
-	// todo: add more command and envs
 	return []corev1.Container{
 		{
-			Name:            "server-executor",
+			Name:            de.Spec.Executor.Name,
 			Image:           de.Spec.Image,
 			ImagePullPolicy: corev1.PullIfNotPresent,
-			Ports:           []corev1.ContainerPort{},
-			Env:             []corev1.EnvVar{},
-			Command:         de.Spec.Executor.Command,
-			VolumeMounts:    []corev1.VolumeMount{},
+			Ports: []corev1.ContainerPort{
+				{
+					Name:          "worker",
+					ContainerPort: de.Spec.Executor.Ports,
+					Protocol:      corev1.ProtocolTCP,
+				},
+			},
+			Env: []corev1.EnvVar{
+				{
+					Name: "POD_HOSTNAME",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.name",
+						},
+					},
+				},
+				{
+					Name:  "EXECUTOR-SERVICE",
+					Value: "server-executor",
+				},
+			},
+			Command: []string{
+				"/df-executor",
+				"--config", "/mnt/config-map/config.toml",
+				"--join", "server-master:10240",
+				"--worker-addr", "0.0.0.0:10241",
+				"--advertise-addr", "${POD_HOSTNAME}.${EXECUTOR-SERVICE}:10241",
+			},
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "dataflow",
+					MountPath: "/tmp/dataflow",
+				},
+				{
+					Name:      "config-map",
+					MountPath: "/mnt/config-map",
+				},
+			},
 		},
 	}
 }
